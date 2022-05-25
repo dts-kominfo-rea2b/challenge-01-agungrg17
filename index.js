b2e310e4a9e9c0ad70162b1a07db2ac2af802498
Agung Rakhmat Gumilar <agungrg17@gmail.com>
2022-05-25 23:46:39 +0700

    membuat challenge data structure di index.js

--- index.js
+++ index.js
@@ -2,10 +2,102 @@
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
 const firstUser = {};
+
+firstUser.name = "Monica";
+
+firstUser.gender = "Female";
+
+firstUser.age = 17;
+
+firstUser.favoriteColor = new Set ();
+firstUser.favoriteColor.add("Yellow");
+firstUser.favoriteColor.add("Pink");
+firstUser.favoriteColor.add("White");
+firstUser.favoriteColor.add("Purple");
+
+firstUser.isHavePet ="Yes";
+
+firstUser.education = [
+    {
+    name : "SD 01",
+    city : "Jakarta",
+    graduate : 2016,
+    },
+    {
+    name : "SMP 02",
+    city : "Jakarta",
+    graduate : 2019,
+    },
+    {
+    name : "SMA 03",
+    city : "Tangerang",
+    }
+];
+
+firstUser.favoriteRestaurant = new Set ();
+firstUser.favoriteRestaurant.add("Bento");
+firstUser.favoriteRestaurant.add("Sushi");
+firstUser.favoriteRestaurant.add("Pancake");
+firstUser.favoriteRestaurant.add("Eggy");
+firstUser.favoriteRestaurant.add("Tempura");
+firstUser.favoriteRestaurant.add("Bento");
+firstUser.favoriteRestaurant.add("Eggy");
+firstUser.favoriteRestaurant.add("Padang");
+firstUser.favoriteRestaurant.add("Tteok");
+firstUser.favoriteRestaurant.add("Sushi");
+firstUser.favoriteRestaurant.add("Sushi");
+
 const secondUser = {};
 
+secondUser.name = "Wendy";
+
+secondUser.gender = "Male";
+
+secondUser.age = 23;
+
+secondUser.favoriteColor = new Set ();
+secondUser.favoriteColor.add("Blue");
+secondUser.favoriteColor.add("Black");
+secondUser.favoriteColor.add("Grey");
+
+secondUser.isHavePet ="No";
+
+secondUser.education = [
+    {
+        name : "SD 02",
+        city : "Jakarta",
+        graduate : 2010,
+        },
+        {
+        name : "SMP 03",
+        city : "Bogor",
+        graduate : 2013,
+        },
+        {
+        name : "SMA 01",
+        city : "Surabaya",
+        graduate : 2016,
+        },
+        {
+        name : "Universitas Maju",
+        city : "Tangerang",
+        }
+];
+
+secondUser.favoriteRestaurant = new Set ();
+secondUser.favoriteRestaurant.add("Tempura");
+secondUser.favoriteRestaurant.add("Bento");
+secondUser.favoriteRestaurant.add("Sushi");
+secondUser.favoriteRestaurant.add("Pancake");
+secondUser.favoriteRestaurant.add("Padang");
+secondUser.favoriteRestaurant.add("Katsu");
+secondUser.favoriteRestaurant.add("Geprek");
+secondUser.favoriteRestaurant.add("Pancake");
+secondUser.favoriteRestaurant.add("Eggy");
+
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+const users = [firstUser, secondUser];
+//console.log(users);
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
