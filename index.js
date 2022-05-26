bf0b7b2f8a692f54a234df248b9f7d21d626a374
Agung Rakhmat Gumilar <agungrg17@gmail.com>
2022-05-26 15:57:30 +0700

    update data array di index.js

--- index.js
+++ index.js
@@ -11,60 +11,61 @@ firstUser.age = 17;
 
 firstUser.email = "monica@dingdong.com";
 
-firstUser.favoriteColor = new Set ();
-firstUser.favoriteColor.add("Yellow");
-firstUser.favoriteColor.add("Pink");
-firstUser.favoriteColor.add("White");
-firstUser.favoriteColor.add("Purple");
-// firstUser.favoriteColor = [];
-// firstUser.favoriteColor.push("Yellow");
-// firstUser.favoriteColor.push("Pink");
-// firstUser.favoriteColor.push("White");
-// firstUser.favoriteColor.push("Purple");
+// firstUser.favoriteColor = new Set ();
+// firstUser.favoriteColor.add("Yellow");
+// firstUser.favoriteColor.add("Pink");
+// firstUser.favoriteColor.add("White");
+// firstUser.favoriteColor.add("Purple");
+firstUser.favoriteColor = [];
+firstUser.favoriteColor.push("Yellow");
+firstUser.favoriteColor.push("Pink");
+firstUser.favoriteColor.push("White");
+firstUser.favoriteColor.push("Purple");
 
-firstUser.isHavePet ="Yes";
+firstUser.isHavePet = true;
 
 firstUser.education = [
     {
     name : "SD 01",
     city : "Jakarta",
-    graduate : 2016,
+    graduate : 2016
     },
     {
     name : "SMP 02",
     city : "Jakarta",
-    graduate : 2019,
+    graduate : 2019
     },
     {
     name : "SMA 03",
     city : "Tangerang",
+    graduate : 0
     }
 ];
 
-firstUser.favoriteRestaurant = new Set ();
-firstUser.favoriteRestaurant.add("Bento");
-firstUser.favoriteRestaurant.add("Sushi");
-firstUser.favoriteRestaurant.add("Pancake");
-firstUser.favoriteRestaurant.add("Eggy");
-firstUser.favoriteRestaurant.add("Tempura");
-firstUser.favoriteRestaurant.add("Bento");
-firstUser.favoriteRestaurant.add("Eggy");
-firstUser.favoriteRestaurant.add("Padang");
-firstUser.favoriteRestaurant.add("Tteok");
-firstUser.favoriteRestaurant.add("Sushi");
-firstUser.favoriteRestaurant.add("Sushi");
-// firstUser.favoriteRestaurant = [];
+// firstUser.favoriteRestaurant = new Set ();
+// firstUser.favoriteRestaurant.add("Bento");
+// firstUser.favoriteRestaurant.add("Sushi");
+// firstUser.favoriteRestaurant.add("Pancake");
+// firstUser.favoriteRestaurant.add("Eggy");
+// firstUser.favoriteRestaurant.add("Tempura");
+// firstUser.favoriteRestaurant.add("Bento");
+// firstUser.favoriteRestaurant.add("Eggy");
+// firstUser.favoriteRestaurant.add("Padang");
+// firstUser.favoriteRestaurant.add("Tteok");
+// firstUser.favoriteRestaurant.add("Sushi");
+// firstUser.favoriteRestaurant.add("Sushi");
+firstUser.favoriteRestaurant = [];
+firstUser.favoriteRestaurant.push("Bento");
+firstUser.favoriteRestaurant.push("Sushi");
+firstUser.favoriteRestaurant.push("Pancake");
+firstUser.favoriteRestaurant.push("Eggy");
+firstUser.favoriteRestaurant.push("Tempura");
 // firstUser.favoriteRestaurant.push("Bento");
-// firstUser.favoriteRestaurant.push("Sushi");
-// firstUser.favoriteRestaurant.push("Pancake");
 // firstUser.favoriteRestaurant.push("Eggy");
-// firstUser.favoriteRestaurant.push("Tempura");
-// firstUser.favoriteRestaurant.push("Bento");
-// firstUser.favoriteRestaurant.push("Eggy");
-// firstUser.favoriteRestaurant.push("Padang");
-// firstUser.favoriteRestaurant.push("Tteok");
+firstUser.favoriteRestaurant.push("Padang");
+firstUser.favoriteRestaurant.push("Tteok");
 // firstUser.favoriteRestaurant.push("Sushi");
-// firstUser.favoriteRestaurant.push("Sushi");
+//  firstUser.favoriteRestaurant.push("Sushi");
 
 const secondUser = {};
 
@@ -76,59 +77,60 @@ secondUser.age = 23;
 
 secondUser.email = "wendy@dingdong.com";
 
-secondUser.favoriteColor = new Set ();
-secondUser.favoriteColor.add("Blue");
-secondUser.favoriteColor.add("Black");
-secondUser.favoriteColor.add("Grey");
-// secondUser.favoriteColor = [];
-// secondUser.favoriteColor.push("Blue");
-// secondUser.favoriteColor.push("Black");
-// secondUser.favoriteColor.push("Grey");
+// secondUser.favoriteColor = new Set ();
+// secondUser.favoriteColor.add("Blue");
+// secondUser.favoriteColor.add("Black");
+// secondUser.favoriteColor.add("Grey");
+secondUser.favoriteColor = [];
+secondUser.favoriteColor.push("Blue");
+secondUser.favoriteColor.push("Black");
+secondUser.favoriteColor.push("Grey");
 
-secondUser.isHavePet ="No";
+secondUser.isHavePet = false;
 
 secondUser.education = [
     {
         name : "SD 02",
         city : "Jakarta",
-        graduate : 2010,
+        graduate : 2010
         },
         {
         name : "SMP 03",
         city : "Bogor",
-        graduate : 2013,
+        graduate : 2013
         },
         {
         name : "SMA 01",
         city : "Surabaya",
-        graduate : 2016,
+        graduate : 2016
         },
         {
         name : "Universitas Maju",
         city : "Tangerang",
+        graduate : 0
         }
 ];
 
-secondUser.favoriteRestaurant = new Set ();
-secondUser.favoriteRestaurant.add("Tempura");
-secondUser.favoriteRestaurant.add("Bento");
-secondUser.favoriteRestaurant.add("Sushi");
-secondUser.favoriteRestaurant.add("Pancake");
-secondUser.favoriteRestaurant.add("Padang");
-secondUser.favoriteRestaurant.add("Katsu");
-secondUser.favoriteRestaurant.add("Geprek");
-secondUser.favoriteRestaurant.add("Pancake");
-secondUser.favoriteRestaurant.add("Eggy");
-// secondUser.favoriteRestaurant = [];
-// secondUser.favoriteRestaurant.push("Tempura");
-// secondUser.favoriteRestaurant.push("Bento");
-// secondUser.favoriteRestaurant.push("Sushi");
-// secondUser.favoriteRestaurant.push("Pancake");
-// secondUser.favoriteRestaurant.push("Padang");
-// secondUser.favoriteRestaurant.push("Katsu");
-// secondUser.favoriteRestaurant.push("Geprek");
-// secondUser.favoriteRestaurant.push("Pancake");
-// secondUser.favoriteRestaurant.push("Eggy");
+// secondUser.favoriteRestaurant = new Set ();
+// secondUser.favoriteRestaurant.add("Tempura");
+// secondUser.favoriteRestaurant.add("Bento");
+// secondUser.favoriteRestaurant.add("Sushi");
+// secondUser.favoriteRestaurant.add("Pancake");
+// secondUser.favoriteRestaurant.add("Padang");
+// secondUser.favoriteRestaurant.add("Katsu");
+// secondUser.favoriteRestaurant.add("Geprek");
+// secondUser.favoriteRestaurant.add("Pancake");
+// secondUser.favoriteRestaurant.add("Eggy");
+secondUser.favoriteRestaurant = [];
+secondUser.favoriteRestaurant.push("Tempura");
+secondUser.favoriteRestaurant.push("Bento");
+secondUser.favoriteRestaurant.push("Sushi");
+secondUser.favoriteRestaurant.push("Pancake");
+secondUser.favoriteRestaurant.push("Padang");
+secondUser.favoriteRestaurant.push("Katsu");
+secondUser.favoriteRestaurant.push("Geprek");
+//secondUser.favoriteRestaurant.push("Pancake");
+secondUser.favoriteRestaurant.push("Eggy");
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [firstUser, secondUser];
